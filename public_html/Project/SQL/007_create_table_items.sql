d43c14ddabe1a221a8a643d48027c49dfe37896e
mm2623 <mm2623@njit.edu>
2021-12-09 13:56:13 -0500

    updated milestone 2

---
+++ public_html/Project/SQL/007_create_table_items.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `Products`
+(
+    `id`           int AUTO_INCREMENT PRIMARY  KEY,
+    `name`          varchar(30) UNIQUE, -- alternatively you'd have a SKU that's unique
+    `description`   text,
+    `category`      text,
+    `stock`         int DEFAULT  0,
+    `cost`    int DEFAULT  99999,
+    `visibility`    text,
+    `image`         text, -- this is the extra feature we have added.
+    `created`       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `modified`      TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+)
--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -3,10 +3,11 @@ require(__DIR__ . "/../../partials/nav.php");
 ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
+    <h1>Login</h1>
         <label for="email">Username/Email</label>
         <input type="text" name="email" required />
     </div>
-    <div> 
+    <div>
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
