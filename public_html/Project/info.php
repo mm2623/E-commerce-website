81877f03b416a8d2b1b0a97e817752c054c9f607
Muhammad Abdul Mannan <90267420+mm2623@users.noreply.github.com>
2022-07-30 12:38:43 -0400

    Update info.php

--- public_html/Project/info.php
+++ public_html/Project/info.php
@@ -12,7 +12,6 @@
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         $result = curl_exec($ch);
         curl_close($ch);
 
